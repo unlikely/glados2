1f1e6fa412c85cc0790ae3350d5e561cb2190000
David Vitrant <david@synteny.us>
2013-09-07 22:01:35 -0400

    git removed the paperwork.* items that got renamed

--- spec/factories/paperwork.rb
+++
@@ -1,9 +0,0 @@
-require 'factory_girl'
-
-FactoryGirl.define do
-  factory :paperwork do
-    sequence(:name, 1000) {|n| "Paperwork#{n}"}
-    sequence(:version, 20)
-    author "Counsel"
-  end
-end
