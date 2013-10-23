8c63344ebe02dc65348149514aa0057690d55db7
David Vitrant <david@synteny.us>
2013-10-23 14:28:31 -0400

    cleaned up some tests in people_controller

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -267,22 +267,9 @@ describe PeopleController do
     end
 
     it "assigns :people with correct contracts on default date" do
-       person     = Person.create(name: "firstnames3")
-       person2    = Person.create(name: "another name")
-       person3    = Person.create(name: "yet another")
-       equipment  = Equipment.create(model: "model3", make: "make3")
-       equipment2  = Equipment.create(model: "mode4", make: "makes4")
-       equipment3  = Equipment.create(model: "new model", make: "new make")
-       expires = Date.today
-       possession_contract = PossessionContract.create(person: person,
-                            equipment: equipment, contract_type: "a sale",
-                            expires: (expires + 10.days))
-       possession_contract2 = PossessionContract.create(person: person,
-                               equipment: equipment2, contract_type: "a sale",
-                               expires: (expires - 10.days))
-       possession_contract3 = PossessionContract.create(person: person3,
-                            equipment: equipment3, contract_type: "borrow",
-                            expires: (expires + 20.days))
+       possession_contract = create(:possession_contract)
+       possession_contract2 = create(:possession_contract)
+       possession_contract3 = create(:possession_contract)
        get :index_equipment_possession_on_date
        assigns(:people).should == [ person3 , person]
     end
