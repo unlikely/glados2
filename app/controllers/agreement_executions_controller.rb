8eb9d18f4253c983b574cd6f4ba31c37760f437b
Matthew Sanabria <MatthewSanabria@gmail.com>
2014-04-29 14:51:31 -0400

    Added Dropbox upload feature

--- app/controllers/agreement_executions_controller.rb
+++ app/controllers/agreement_executions_controller.rb
@@ -1,3 +1,4 @@
+require 'mime/types'
 class AgreementExecutionsController < ApplicationController
   respond_to :html, :json
 
@@ -20,32 +21,23 @@ class AgreementExecutionsController < ApplicationController
   def create  
     @client = Dropbox::API::Client.new(:token  => 'nqwavyitn2wvab9k', :secret => '0o5isnlrhjcodi2')
     @picture = params[:agreement_execution][:picture]
-    @url = params[:agreement_execution][:url]
+    @url = params[:agreement_execution][:agreement_url]
     @form_data = params[:agreement_execution].select{|key, value| key != "picture"}
     @agreement_execution = AgreementExecution.new(@form_data)
-    @continue = false 
       
-      if @picture && @url 
+      if @picture.present? && @url.present?
         flash[:error] = "Please include only one of the following:  DropBox URL or Upload"
-        render 'new'
-      else
-        @continue = true
+      elsif @picture.present? && @url.blank?
+        @client_file = @client.upload(dropbox_filename(@agreement_execution, @picture), @picture.read)
+        @agreement_execution.update_attributes(agreement_url: @client_file.direct_url.url)
+        if @agreement_execution.save
+          flash[:success] = "Agreement successfully signed"
+          redirect_to agreement_executions_path
+        else
+          flash.now[:error] = "Agreement signature not correctly saved"
+          render 'new'
+        end
       end 
-
-      if @picture && @continue
-        @client_file = @client.upload(dropbox_filename(@agreement_execution), @picture.read)
-        @agreement_execution.agreement_url = @client_file.direct_url.url
-      elsif @url && @continue 
-        @agreement_execution.agreement_url = @url
-      end   
-
-      if @agreement_execution.save
-        flash[:success] = "Agreement successfully signed"
-        redirect_to agreement_executions_path
-      else
-        flash.now[:error] = "Agreement signature not correctly saved"
-        render 'new'
-      end
   end
 
   def edit
@@ -75,11 +67,14 @@ class AgreementExecutionsController < ApplicationController
     else
       flash[:error] = "Agreement execution could not be deleted"
       redirect_to request.referrer
-    end
+    end 
   end
 
  private 
- def dropbox_filename(agreement_execution)
-  "#{agreement_execution.date_signed.strftime('%Y_%m_%d')}_#{agreement_execution.person.name}"
- end 
+  def dropbox_filename(agreement_execution, picture)
+     @ext= Rack::Mime::MIME_TYPES.invert[picture.content_type]
+    "#{agreement_execution.date_signed.strftime('%Y_%m_%d')}_#{agreement_execution.person.first_name}_#{agreement_execution.person.last_name}#{@ext}"
+  end 
+
 end
+
